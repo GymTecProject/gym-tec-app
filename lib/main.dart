f3dfd060320e8c2b0de427e35219123b4ca8bff8
Corne1401 <47864642+danielcornejo14@users.noreply.github.com>
2023-09-11 18:08:12 -0600

    Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gym_tec/auth/auth.dart';
 import 'package:gym_tec/auth/login.dart';
@@ -6,7 +9,27 @@ import 'package:gym_tec/pages/admin/admin_root_page.dart';
 import 'package:gym_tec/pages/trainer/trainer_root_page.dart';
 import 'package:gym_tec/pages/user/user_root_page.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // run emulator if debug mode is on
+  // firebase emulators:start
+  if (kDebugMode) {
+    try {
+      FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+      await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
+  }
+
   runApp(const MyApp());
 }
 
@@ -19,33 +42,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: const ColorScheme(
-            brightness: Brightness.light,
-            primary: Color(0xFFa2e200),
-            onPrimary: Color.fromARGB(255, 0, 0, 0),
-            secondary: Colors.purple,
-            onSecondary: Colors.black,
-            error: Colors.red,
-            onError: Colors.white,
-            background: Colors.black,
-            onBackground: Colors.white,
-            surface: Color(0xFF424242),
-            onSurface: Colors.white,),
+          brightness: Brightness.light,
+          primary: Color(0xFFa2e200),
+          onPrimary: Color.fromARGB(255, 0, 0, 0),
+          secondary: Colors.purple,
+          onSecondary: Colors.black,
+          error: Colors.red,
+          onError: Colors.white,
+          background: Colors.black,
+          onBackground: Colors.white,
+          surface: Color(0xFF424242),
+          onSurface: Colors.white,
+        ),
         useMaterial3: true,
       ),
       initialRoute: '/',
@@ -60,4 +69,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
