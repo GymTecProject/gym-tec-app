4bd13dd14ea4d5a0a99b1108d7c23814516700c3
JGamboaB <jgamboabotero@gmail.com>
2023-09-15 09:07:37 -0600

    Minor changes added to user_search

--- lib/pages/trainer/trainer_page/search_user.dart
+++ lib/pages/trainer/trainer_page/search_user.dart
@@ -9,16 +9,67 @@ class SearchUser extends StatefulWidget {
 
 class _SearchUserState extends State<SearchUser> {
   final List<Map<String, dynamic>> _allUsers = [
-    {"id": 1, "name": "Andy", "age": 29},
-    {"id": 2, "name": "Aragon", "age": 40},
-    {"id": 3, "name": "Bob", "age": 5},
-    {"id": 4, "name": "Barbara", "age": 35},
-    {"id": 5, "name": "Candy", "age": 21},
-    {"id": 6, "name": "Colin", "age": 55},
-    {"id": 7, "name": "Audra", "age": 30},
-    {"id": 8, "name": "Banana", "age": 14},
-    {"id": 9, "name": "Caversky", "age": 100},
-    {"id": 10, "name": "Becky", "age": 32},
+    // UserPublicData
+    {
+      "id": 1,
+      "name": "Andy",
+      "phone": "555-123-4567",
+      "email": "andy@example.com"
+    },
+    {
+      "id": 2,
+      "name": "Aragon",
+      "phone": "555-987-6543",
+      "email": "aragon@example.com"
+    },
+    {
+      "id": 3,
+      "name": "Bob",
+      "phone": "555-555-5555",
+      "email": "bob@example.com"
+    },
+    {
+      "id": 4,
+      "name": "Barbara",
+      "phone": "555-111-2222",
+      "email": "barbara@example.com"
+    },
+    {
+      "id": 5,
+      "name": "Candy",
+      "phone": "555-333-4444",
+      "email": "candy@example.com"
+    },
+    {
+      "id": 6,
+      "name": "Colin",
+      "phone": "555-666-7777",
+      "email": "colin@example.com"
+    },
+    {
+      "id": 7,
+      "name": "Audra",
+      "phone": "555-999-0000",
+      "email": "audra@example.com"
+    },
+    {
+      "id": 8,
+      "name": "Banana",
+      "phone": "555-888-5555",
+      "email": "banana@example.com"
+    },
+    {
+      "id": 9,
+      "name": "Caversky",
+      "phone": "555-222-1111",
+      "email": "caversky@example.com"
+    },
+    {
+      "id": 10,
+      "name": "Becky",
+      "phone": "555-777-3333",
+      "email": "becky@example.com"
+    },
   ];
 
   List<Map<String, dynamic>> _foundUsers = [];
@@ -86,9 +137,10 @@ class _SearchUserState extends State<SearchUser> {
                     elevation: 4,
                     margin: const EdgeInsets.symmetric(vertical: 10),
                     child: ListTile(
-                      leading: Text(_foundUsers[index]['id'].toString()),
+                      //leading: Text(_foundUsers[index]['id'].toString()),
                       title: Text(_foundUsers[index]['name']),
-                      subtitle: Text(_foundUsers[index]['age'].toString()),
+                      subtitle: Text(
+                          "Phone: ${_foundUsers[index]['phone'].toString()} - Email: ${_foundUsers[index]['email']}"),
                     ),
                   ),
                 ),
@@ -106,7 +158,7 @@ class _SearchUserState extends State<SearchUser> {
           title: Text("Client: ${s['name']}"),
           content: Column(mainAxisSize: MainAxisSize.min, children: [
             const SizedBox(height: 10),
-            Text("Age: ${s['age']}"),
+            Text("Phone: ${s['phone']}"),
           ]),
           actions: [
             Row(
